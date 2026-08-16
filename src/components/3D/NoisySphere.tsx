65b3a03fcadbf32ddd5b88a00a44d4943db086ba
Shane Cranor <70922464+shanecranor@users.noreply.github.com>
2026-08-15 20:50:24 -0500

    messing around with materials

--- src/components/3D/NoisySphere.tsx
+++ src/components/3D/NoisySphere.tsx
@@ -2,6 +2,7 @@ import { useMemo, useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { simplexNoise3D } from "./simplex";
+import { MeshTransmissionMaterial } from "@react-three/drei";
 
 function simplexNoise3DFractal(
   x: number,
@@ -217,21 +218,21 @@ export function NoisySphere({
         // scale={[0.998, 0.998, 0.998]}
         scale={0.999}
       >
-        <meshBasicMaterial color={[0.015, 0.005, 0.01]} />
-        {/* <MeshTransmissionMaterial
+        {/* <meshBasicMaterial color={[0.015, 0.005, 0.01]} /> */}
+        <MeshTransmissionMaterial
           backside
-          samples={4}
-          resolution={256}
+          samples={2}
+          resolution={1024}
           thickness={0.05}
-          roughness={0.1}
+          roughness={0.8}
           iridescence={1}
           iridescenceIOR={1.4}
           chromaticAberration={1}
           anisotropy={1}
-          color={[0.2, 0.2, 0.2]}
-          transmission={0.3}
+          color={[1, 1, 1]}
+          transmission={0.2}
           emissive={[0, 0, 0]}
-        /> */}
+        />
       </mesh>
       {edgeColor && (
         <lineSegments geometry={wireframeGeometry}>
